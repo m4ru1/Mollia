c9aa67eb0d3372ef5d2cf1727b5dba0db786da7b
mollia <m4ru1@qq.com>
2023-09-02 22:57:35 +0800

    fixbugs: route config error with nested-routes.

--- mollia-console/src/router/index.js
+++ mollia-console/src/router/index.js
@@ -5,63 +5,43 @@ const router = new createRouter({
     routes: [
         {
             path: '/',
-            component: () => import('../views/home/Home.vue'),
-            children: [
-                {
-                    path: 'article',
-                    component: () => import('../views/notfound/NotFound.vue'),
-                    children: [
-                        {
-                            path: 'create',
-                            component: () => import('../views/article/ArticleEditor.vue')
-                        },
-                        {
-                            path: ':id',
-                            component: () => import('../views/article/ArticleEditor.vue')
-                        },
-                        {
-                            path: 'list',
-                            component: () => import('../views/article/ArticleCategory.vue')
-                        },
-                        {
-                            path: 'category',
-                            component: () => import('../views/article/ArticleCategory.vue')
-                        }
-                    ]
-                },
-                {
-                    path: 'option',
-                    component: () => import('../views/notfound/NotFound.vue'),
-                    children: [
-                        {
-                            path: 'theme',
-                            component: () => import('../views/option/ThemeOption.vue'),
-                        },
-                        {
-                            path: 'profile',
-                            component: () => import('../views/option/ProfileOption.vue'),
-                        },
-                        {
-                            path: 'about',
-                            component: () => import('../views/option/AboutOption.vue'),
-                        },
-                    ]
-                },
-                {
-                    path: 'storage',
-                    component: () => import('../views/notfound/NotFound.vue'),
-                    children: [
-                        {
-                            path: 'image',
-                            component: () => import('../views/storage/ImageStorage.vue'),
-                        },
-                        {
-                            path: 'attach',
-                            component: () => import('../views/storage/AttachStorage.vue')
-                        }
-                    ]
-                }
-            ],
+            component: () => import('../views/home/Home.vue')
+        },
+        {
+            path: '/article/create',
+            component: () => import('../views/article/ArticleEditor.vue')
+        },
+        {
+            path: '/article/:id',
+            component: () => import('../views/article/ArticleEditor.vue')
+        },
+        {
+            path: '/article/list',
+            component: () => import('../views/article/ArticleList.vue')
+        },
+        {
+            path: '/article/category',
+            component: () => import('../views/article/ArticleCategory.vue')
+        },
+        {
+            path: '/option/theme',
+            component: () => import('../views/option/ThemeOption.vue'),
+        },
+        {
+            path: '/option/profile',
+            component: () => import('../views/option/ProfileOption.vue'),
+        },
+        {
+            path: '/option/about',
+            component: () => import('../views/option/AboutOption.vue'),
+        },
+        {
+            path: '/storage/image',
+            component: () => import('../views/storage/ImageStorage.vue'),
+        },
+        {
+            path: '/storage/attach',
+            component: () => import('../views/storage/AttachStorage.vue')
         }
     ]
 });
